d0854617012f231018984b7e31021995d0d26ac9
anutaguzova <anuta-guzova@mail.ru>
2021-04-10 14:09:41 +0300

    feat: add solution 9

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -9,9 +9,17 @@
  * For 100, the result should be 1 (1 + 0 + 0 = 1)
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
- */
+*/
+function sum(number) {
+  return String(number).split('').map((el) => +el).reduce((a, b) => a + b);
+}
+
 function getSumOfDigits(n) {
-  return n.toString().split().reduce((a, b) => a + b);
+  let result = n;
+  while (result > 9) {
+    result = sum(result);
+  }
+  return result;
 }
 
 module.exports = getSumOfDigits;
