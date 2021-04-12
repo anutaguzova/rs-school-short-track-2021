ce44dd8a5c362a0e2cb192ab79418bbe57acdd3b
anutaguzova <anuta-guzova@mail.ru>
2021-04-12 14:41:12 +0300

    feat: add solution task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,26 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+  for (let i = 0; i < rows; i++) {
+    const line = [];
+    for (let j = 0; j < columns; j++) {
+      let count = 0;
+      if (i > 0 && matrix[i - 1][j] === true) count += 1;
+      if (i < (rows - 1) && matrix[i + 1][j] === true) count += 1;
+      if (i > 0 && j < (columns - 1) && matrix[i - 1][j + 1] === true) count += 1;
+      if (i > 0 && j > 0 && matrix[i - 1][j - 1] === true) count += 1;
+      if (i < (rows - 1) && j < (columns - 1) && matrix[i + 1][j + 1] === true) count += 1;
+      if (i < (rows - 1) && j > 0 && matrix[i + 1][j - 1] === true) count += 1;
+      if (j > 0 && matrix[i][j - 1] === true) count += 1;
+      if (j < (columns - 1) && matrix[i][j + 1] === true) count += 1;
+      line.push(count);
+    }
+    result.push(line);
+  } return result;
 }
 
 module.exports = minesweeper;
